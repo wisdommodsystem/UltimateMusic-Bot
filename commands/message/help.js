e27003f88eec0c8b12a2109debd930305990eed8
wisdommodsystem <am.or79h@gmail.com>
2025-09-16 19:49:41 +0100

    Update help.js

--- commands/message/help.js
+++ commands/message/help.js
@@ -60,7 +60,7 @@ module.exports = {
                 .setTitle('📖 Ultimate Music Bot - Command List')
                 .setColor(0x1DB954)
                 .setDescription(description)
-                .setFooter({ text: 'Developed by GlaceYT | https://glaceyt.com' })
+                .setFooter({ text: 'Developed by Apollo  | For Wisdom Circle ⭕' })
                 .setTimestamp();
 
             await message.reply({ embeds: [embed] });
@@ -71,3 +71,4 @@ module.exports = {
         }
     }
 };
+
