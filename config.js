2220023e160971e0de1452c12b7d2cb03906b32c
wisdommodsystem <am.or79h@gmail.com>
2025-09-17 00:38:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "173.249.0.115", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 13592,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "https://camming.xyz", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "5.39.63.207", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 8262,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
