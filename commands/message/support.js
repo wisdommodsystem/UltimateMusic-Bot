fc4818c11445467b0411d1cf229fa0a4816bb9d1
wisdommodsystem <am.or79h@gmail.com>
2025-09-16 21:02:56 +0100

    Update support.js

--- commands/message/support.js
+++ commands/message/support.js
@@ -25,12 +25,12 @@ module.exports = {
                 .setColor(0x1DB954)
                 .setDescription(
                     'Need help or have questions? Join our official support server:\n' +
-                    '[Support Server](https://discord.gg/xQF9f9yUEM)\n\n' +
-                    'For direct inquiries, contact: **GlaceYT**\n\n' +
-                    'Website: https://glaceyt.com'
+                    '[Support Server](https://discord.gg/r3HGHr6HCW)\n\n' +
+                    'For direct inquiries, contact: **Apollo Mazigh**\n\n' +
+                    'Website: www.malahida.com'
                 )
                 .setTimestamp()
-                .setFooter({ text: 'Ultimate Music Bot • Developed by GlaceYT' });
+                .setFooter({ text: 'MelodyAI • Developed by Apollo' });
             
             await message.reply({ embeds: [embed] });
         } catch (error) {
@@ -39,3 +39,4 @@ module.exports = {
         }
     }
 };
+
