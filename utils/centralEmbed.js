45d5d5bf1db3eedecc8681227791481e2d214e1c
wisdommodsystem <am.or79h@gmail.com>
2025-09-17 22:15:33 +0100

    Update centralEmbed.js

--- utils/centralEmbed.js
+++ utils/centralEmbed.js
@@ -209,7 +209,7 @@ class CentralEmbedHandler {
                     .setThumbnail(trackInfo.thumbnail || 'https://cdn.discordapp.com/emojis/896724352949706762.gif')
                     .setImage(trackInfo.paused ? null : 'https://i.ibb.co/KzbPV8jd/aaa.gif')
                     .setFooter({ 
-                        text: `Ultimate Music Bot • ${statusText} Developed By GlaceYT`,
+                        text: `MelodyAI BOT • ${statusText} Developed By Apollo 💎`,
                         iconURL: this.client.user.displayAvatarURL()
                     })
                     .setTimestamp();
@@ -365,5 +365,6 @@ class CentralEmbedHandler {
 }
 
 module.exports = CentralEmbedHandler;
+
 
 
